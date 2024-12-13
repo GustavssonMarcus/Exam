8c92c53505603d119b7d969244a35987d513b05e
Marcus Gustavsson <mackan1892@outlook.com>
2024-12-13 11:43:58 +0100

    get test data from mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,19 @@
 require('dotenv').config();
 
-// const connectToDatabase = require('./db');
-
-// const cors = require('cors');
-// const bodyParser = require("body-parser");
-//import express from "express";
-//import mongoose from "mongoose";
 const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
 
+//Hämtar variabler från .env filen
 const PORT = process.env.PORT || 7000;
 const MONGOURL = process.env.MONGO_URL;
 
 if (!MONGOURL) {
   console.error("MONGO_URL är undefined. Kontrollera din .env-fil.");
-  process.exit(1); // Avsluta processen om URI saknas
+  process.exit(1);
 }
-
+//Koppling mellan mongodb och vs code
 mongoose.connect(MONGOURL)
   .then(() => {
     console.log("Connected to MongoDB!");
@@ -30,22 +25,20 @@ mongoose.connect(MONGOURL)
     console.error("Misslyckades att ansluta till MongoDB:", err.message);
   });
 
+//Hämtar data från mongodb
+  const userSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+  });
 
+  const UserModel = mongoose.model("products", userSchema)
 
-// connectToDatabase();
-
-// app.use(cors());
-// app.use(express.json());
-// app.use(bodyParser.json());
+  app.get('/getProducts', async(req, res) => {
+    const userData = await UserModel.find();
+      res.json(userData);
+    });
 
 //route
 app.get('/', (req, res) => {
     res.send('Backend är igång');
 });
-
-app.get('/data', (req, res) => {
-    res.json({ message: 'Detta är data från backend' });
-  });
-
-//Starta route
-// app.listen(PORT, () => console.log(`Server är igång på port ${PORT}`));
