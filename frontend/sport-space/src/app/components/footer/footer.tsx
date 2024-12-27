c368c6e2e76e8ddcf9ff812e0a4fd3846dd1580f
Marcus Gustavsson <mackan1892@outlook.com>
2024-12-27 09:51:29 +0100

    Content on footer

--- frontend/sport-space/src/app/components/footer/footer.tsx
+++ frontend/sport-space/src/app/components/footer/footer.tsx
@@ -2,8 +2,30 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-white p-4">
-      <p>&copy; {new Date().getFullYear()} My Application. All rights reserved.</p>
+    <footer className='footer'>
+      <div className='footer-content'>
+        <div className='footer-content-top'>
+          <div className='footer-content-top-visit'>
+            <p>Besök oss</p>
+          </div>
+          <div className='footer-content-top-contact'>
+            <p>Kontakta oss</p>
+          </div>
+        </div>
+        <div className='footer-content-middle'>
+          <ul>
+            <li>Google</li>
+            <li>Instagram</li>
+            <li>Facebook</li>
+          </ul>
+        </div>
+        <div className='footer-content-bottom'>
+          <p>&copy; {new Date().getFullYear()} Sport Space</p>
+        </div>
+      </div>
+      <div className='footer-title'>
+        <h1>Sport Space</h1>
+      </div>
     </footer>
   );
 };
