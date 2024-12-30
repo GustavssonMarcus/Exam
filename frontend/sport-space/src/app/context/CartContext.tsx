c44199cac60aed629d82a0be9908d8d7e9af9d4a
Marcus Gustavsson <mackan1892@outlook.com>
2024-12-30 11:33:15 +0100

    Local storage för att spara vid omladdning

--- frontend/sport-space/src/app/context/CartContext.tsx
+++ frontend/sport-space/src/app/context/CartContext.tsx
@@ -1,7 +1,6 @@
 "use client";
-import { createContext, useContext, useState, ReactNode } from 'react';
+import { createContext, useContext, useState, ReactNode, useEffect } from 'react';
 import { Product } from '../modules/products';
-
 
 type CartContextType = {
   cart: Product[];
@@ -12,6 +11,19 @@ const CartContext = createContext<CartContextType | undefined>(undefined);
 
 export const CartProvider = ({ children }: { children: ReactNode }) => {
   const [cart, setCart] = useState<Product[]>([]);
+
+  // Hämta kundvagn från localStorage vid sidladdning
+  useEffect(() => {
+    const storedCart = localStorage.getItem('cart');
+    if (storedCart) {
+      setCart(JSON.parse(storedCart));
+    }
+  }, []);
+
+  // Spara kundvagn i localStorage varje gång den uppdateras
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }, [cart]);
 
   const addToCart = (product: Product) => {
     setCart((prevCart) => [...prevCart, product]);
