c63d9cb8d77b795ba016db56d6b4378cbac37dab
Marcus Gustavsson <mackan1892@outlook.com>
2025-01-08 10:32:15 +0100

    Contact form

--- frontend/sport-space/src/app/kontakt/page.tsx
+++ frontend/sport-space/src/app/kontakt/page.tsx
@@ -1,3 +1,75 @@
+'use client';
+
+import { useState, ChangeEvent, FormEvent } from "react";
+
 export default function Page() {
-    return <p>Kontakta oss</p>;
-  }
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log("Form data submitted:", formData);
+    setSubmitted(true);
+
+    setFormData({
+      name: "",
+      email: "",
+      message: "",
+    });
+  };
+
+  return (
+    <div className="contact">
+     <h1>Kontakta oss</h1>
+      {submitted && <p>Tack för ditt meddelande! Vi återkommer så snart som möjligt.</p>}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Namn:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">E-post:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Meddelande:</label>
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            required
+          ></textarea>
+        </div>
+        <button type="submit">Skicka</button>
+      </form>
+    </div>
+  );
+}
