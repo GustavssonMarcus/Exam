4ab065b7e2e8e87104891129e252f2e48a72759e
Marcus Gustavsson <mackan1892@outlook.com>
2025-01-10 11:47:25 +0100

    Success page

--- frontend/sport-space/src/app/success/page.tsx
+++ frontend/sport-space/src/app/success/page.tsx
@@ -1,3 +1,24 @@
-export default function Page() {
-    return <p>Success</p>;
-  }
+"use client";
+
+import Link from 'next/link';
+
+export default function SuccessPage() {
+  return (
+    <div className="success">
+      <div className="success-message">
+        <h1>Tack för din beställning!</h1>
+        <p>Din beställning har lagts och vi arbetar nu med att förbereda den för leverans.</p>
+        <p>Du kommer att få en bekräftelse via e-post inom kort.</p>
+      </div>
+      
+      <div className="success-actions">
+        <Link className="success-actions-link" href="/">
+          <button className="success-actions-link-btn">Tillbaka till startsidan</button>
+        </Link>
+        <Link className="success-actions-link" href="/sortiment">
+          <button className="success-actions-link-btn">Fortsätt handla</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
